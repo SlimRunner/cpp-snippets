a4436ad180d0b6cd32aae8dc2cee642565999833
SlimRunner <dv.polygon@gmail.com>
2024-08-12 16:46:35 -0700

    Update ArgParser to most current version

--- argument-parser/include/ArgParser.hpp
+++ argument-parser/include/ArgParser.hpp
@@ -11,28 +11,38 @@ using argmap = std::map<std::string, vecstring>;
 class ArgParser {
 private:
   argmap m_args;
-  bool m_matched;
 
 public:
   ArgParser(/* args */) = delete;
 
   ArgParser(int argc, char const *argv[]);
 
-  vecstring getKeys();
+  /// @brief get value of first argument of given flag
+  /// @param flag argument flag to search for
+  /// @return string of frist argument
+  std::string findFirst(std::string flag) const;
 
-  std::string findFirst(std::string key);
+  bool findFirst(std::string flag, std::string & out) const;
 
-  vecstring findParams(std::string key);
+  /// @brief get list of arguments of given flag
+  /// @param flag argument flag to search for
+  /// @return vector of arguments
+  vecstring findParams(std::string flag) const;
 
-  bool hasKey(std::string key);
+  bool findParams(std::string flag, vecstring & out) const;
 
-  /**
-   * @brief Determines the result of last search performed
-   * 
-   * @return true if last search succeeded
-   * @return false if last search failed
-   */
-  inline bool found() { return m_matched; };
+  /// @brief determines if provided value exists as an argument flag
+  /// @param flag argument flag to search for
+  /// @return true if key was found, false otherwise
+  bool hasFlag(std::string flag) const;
+
+  /// @brief determines if underlying list is empty
+  /// @return true if list is empty, false otherwise
+  inline bool empty() const { return m_args.empty(); }
+
+  argmap::const_iterator begin() const;
+
+  argmap::const_iterator end() const;
 };
 
 } // namespace System
--- argument-parser/src/ArgParser.cpp
+++ argument-parser/src/ArgParser.cpp
@@ -2,11 +2,11 @@
 
 namespace System {
 ArgParser::ArgParser(int argc, char const *argv[]) :m_args() {
-  if (argc == 1) {
+  if (argc <= 0) {
     return;
   }
   std::string keys = "";
-  vecstring params;
+  vecstring params = {{argv[0]}};
   for (int i = 1; i < argc; ++i) {
     if (argv[i][0] == '-') {
       m_args.emplace(keys, params);
@@ -19,32 +19,40 @@ ArgParser::ArgParser(int argc, char const *argv[]) :m_args() {
   m_args.emplace(keys, params);
 }
 
-vecstring ArgParser::getKeys() {
-  vecstring output;
-  output.reserve(m_args.size());
-  for (const auto &arg: m_args) {
-    output.push_back(arg.first);
-  }
-  return output;
-}
-
-std::string ArgParser::findFirst(std::string key) {
-  if (hasKey(key)) {
-    return m_args.at(key).front();
+std::string ArgParser::findFirst(std::string flag) const {
+  if (hasFlag(flag)) {
+    return m_args.at(flag).front();
   }
   return std::string();
 }
 
-vecstring ArgParser::findParams(std::string key) {
-  if (hasKey(key)) {
-    return m_args.at(key);
+bool ArgParser::findFirst(std::string flag, std::string &out) const {
+  out = findFirst(flag);
+  return hasFlag(flag);
+}
+
+vecstring ArgParser::findParams(std::string flag) const {
+  if (hasFlag(flag)) {
+    return m_args.at(flag);
   }
   return vecstring();
 }
 
-bool ArgParser::hasKey(std::string key) {
-  m_matched = m_args.find(key) != m_args.end();
-  return m_matched;
+bool ArgParser::findParams(std::string flag, vecstring &out) const {
+  out = findParams(flag);
+  return hasFlag(flag);
+}
+
+bool ArgParser::hasFlag(std::string flag) const {
+  return m_args.find(flag) != m_args.end();
+}
+
+argmap::const_iterator ArgParser::begin() const {
+  return m_args.begin();
+}
+
+argmap::const_iterator ArgParser::end() const {
+  return m_args.end();
 }
 
 } // namespace System
