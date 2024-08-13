9f2ff5e813e2a0e35a4fca636702ad876d662126
SlimRunner <dv.polygon@gmail.com>
2024-08-13 02:27:40 -0700

    Improved timing for empty loop

--- emtpy-iter/main.cpp
+++ emtpy-iter/main.cpp
@@ -1,25 +1,8 @@
-#include <cstddef> // size_t
-#include <iomanip>
-#include <iostream>
-#include <chrono>
-
-int main(int argc, char const *argv[]) {
-  using steady = std::chrono::steady_clock;
-  using chrono_us = std::chrono::milliseconds;
-
-  auto start = steady::now();
-
-  size_t i = 0, count = 0, printLimit = 1;
-  while (count <= 1) {
-    if (i >= printLimit) {
-      auto stamp = std::chrono::duration_cast<chrono_us>(steady::now() - start).count();
-      std::cout << std::setw(7) << stamp << std::setw(21) << i << '\n';
-      printLimit <<= 1;
-    }
-    ++i;
-    if (i == 0) {
-      ++count;
-    }
-  }
+#include <cstdint>
+#include <string>
+int main(int argc, char const **argv) {
+  uint64_t max;
+  max = (argc<=1?UINT64_MAX:std::stoll(argv[1]));
+  for (uint64_t i = 0; i < max; ++i) {}
   return 0;
 }
