48f320bdc73be7cb53222c0e6ba4c2ed6373b536
SlimRunner <dv.polygon@gmail.com>
2025-10-10 20:41:54 -0700

    Simplify quick cpp profiler

--- simple-profiler/main.cpp
+++ simple-profiler/main.cpp
@@ -8,13 +8,8 @@ int foobar(int a, bool b) { return b ? a : -a; }
 int foo() { return 42; }
 
 int main(int argc, char const *argv[]) {
-  using chrono_us = std::chrono::milliseconds;
-  constexpr auto to_ms = [](auto x) {
-    return std::chrono::duration_cast<chrono_us>(x).count();
-  };
-
-  std::cout << to_ms(timeIt<0x200000>(foo)) << "\n";
-  std::cout << to_ms(timeIt<0x200000>(foobar, 5, false)) << "\n";
+  std::cout << timeIt<0x1000000>(foo) << "\n";
+  std::cout << timeIt<0x1000000>(foobar, 5, false) << "\n";
 
   return 0;
 }
--- simple-profiler/profiler.hpp
+++ simple-profiler/profiler.hpp
@@ -3,17 +3,17 @@
 #include <functional>
 
 template <int T, class F, class... Args> auto timeIt(F func, Args... args) {
-  using hr_clock = std::chrono::steady_clock;
+  using hr_clock = std::chrono::high_resolution_clock;
+  using chrono_ms = std::chrono::milliseconds;
+  constexpr auto to_ms = [](auto x) {
+    return std::chrono::duration_cast<chrono_ms>(x).count();
+  };
+
   auto start = hr_clock::now();
 
-  for (size_t i = 0; i < T; i++) {
-    if constexpr (std::is_void_v<std::invoke_result_t<F, Args...>>) {
-      std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-    } else {
-      // result is discarded
-      std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
-    }
+  for (size_t i = 0; i < T; ++i) {
+    std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
   }
 
-  return hr_clock::now() - start;
+  return to_ms(hr_clock::now() - start);
 }
